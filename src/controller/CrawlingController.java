040c83d4e2ccd887001ab6f736c6adad69552cf0
kimsintae <kimstcool01@naver.com>
2018-12-06 18:44:19 +0900

    c

--- src/controller/CrawlingController.java
+++ src/controller/CrawlingController.java
@@ -20,6 +20,7 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -54,8 +55,12 @@ public class CrawlingController implements Initializable{
 	ObservableList<TotalSearchData> resultList = FXCollections.observableArrayList();
 	
 	Properties pros = new Properties();
+	
+	// 값 초기 설정
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		//선택 박스 값  설정
 		choiceList.removeAll(choiceList);
 		choiceList.add("MOVIE");
 		choiceList.add("TRAVEL");
@@ -63,31 +68,43 @@ public class CrawlingController implements Initializable{
 		choiceList.add("CAR");
 		choiceList.add("SOCCER");
 		type.getItems().addAll(choiceList);
-		
-	 TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-			@Override
-			public X509Certificate[] getAcceptedIssuers() {
-				return new X509Certificate[0];
+		type.getSelectionModel().select(0); // 처음 선택 되어 있을 값
+		// https 연결 설정	
+		 TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+				@Override
+				public X509Certificate[] getAcceptedIssuers() {
+					return new X509Certificate[0];
+				}
+				
+				@Override
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+				
+				@Override
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+			 }
+			};
+		 try {
+			 pros.load(new FileInputStream("site_info.properties"));
+		     SSLContext sc = SSLContext.getInstance("TLS");
+				sc.init(null, trustAllCerts, new SecureRandom());
+				HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			} catch (KeyManagementException | NoSuchAlgorithmException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-			@Override
-			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		 
+		 
+		 
+		 
+		 
+		 type.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+			 @Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				 System.out.println();
 			}
-			
-			@Override
-			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			}
-		 }
-		};
-	 try {
-		 pros.load(new FileInputStream("site_info.properties"));
-	     SSLContext sc = SSLContext.getInstance("TLS");
-			sc.init(null, trustAllCerts, new SecureRandom());
-			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		} catch (KeyManagementException | NoSuchAlgorithmException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		});
 	
 	}//init
 
@@ -105,7 +122,7 @@ public class CrawlingController implements Initializable{
 			//크롤링시작
 			if(chk.isSelected()){
 				if(!keyword.getText().trim().equals("")&&typeName!=null){
-					resultList.addAll(CrawlingModule.doCrawling(pros.getProperty(chk.getId()+"_"+type.getValue()),keyword.getText().trim(),chk.getId()));
+					resultList.addAll(CrawlingModule.doCrawling(pros.getProperty(chk.getId()+"_"+type.getValue().toLowerCase()),keyword.getText().trim(),chk.getId()));
 				}else{
 					System.out.println("검색어 없거나 타입 선택되지 않음");
 				}
@@ -131,4 +148,5 @@ public class CrawlingController implements Initializable{
     public void getEndDate(ActionEvent event){
  	    System.out.println(endDate.getValue());
     }
+    
 }
